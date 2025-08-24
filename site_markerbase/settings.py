ab15e3f83b57c6767daafc384a6d982f1522a5be
greengooscenario <pggs@posteo.de>
2025-08-25 00:29:36 +0200

    Initial commit

--- site_markerbase/settings.py
+++ site_markerbase/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Peculiarities for TextsInSpace
+# General path configuration
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 MEDIA_URL = '/files/'
